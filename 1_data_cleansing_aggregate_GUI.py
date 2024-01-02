706f208e1fb3fddfa2e9429e71587e46018b50e0
JuMpYggdrasil <70381321+JuMpYggdrasil@users.noreply.github.com>
2024-01-02 20:40:59 +0700

    update

---
+++ 1_data_cleansing_aggregate_GUI.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+from tkinter import filedialog
+from data_cleansing_aggregate import load_and_process_data
+
+# Create a function to handle file selection
+def select_csv_file():
+    file_path = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv")])
+    if file_path:
+        load_and_process_data(file_path)
+        print("fp:")
+        print(type(file_path))
+        print(file_path)
+
+
+
+# Create the main application window
+root = tk.Tk()
+root.title("CSV File Selector")
+
+
+# Create a label and place it above the button
+label = tk.Label(root, text="Show Peak & Light Day Pattern")
+label.pack(pady=(20, 0))  # Adding some padding for better spacing
+
+
+# Create a button to open the file dialog
+select_button = tk.Button(root, text="Select .csv File", command=select_csv_file)
+select_button.pack(pady=20)
+
+# Start the Tkinter main loop
+root.mainloop()
+
