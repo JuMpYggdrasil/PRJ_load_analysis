61415de2f057e7cb6a6c4717eb5ef38e77e9d071
JuMpYggdrasil <70381321+JuMpYggdrasil@users.noreply.github.com>
2024-01-26 14:23:14 +0700

    update

--- 4_load_pv_batt.py
+++ 4_load_pv_batt.py
@@ -2,7 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-PV_Install_Capacity = [1,50,100,150] # kW
+PV_Install_Capacity = [1,150] # kW
+
+unit_price_on_peak = 4.1839
+unit_price_off_peak = 2.6037
+# unit_price_holiday = unit_price_off_peak
+unit_price_demand_charge = 132.93
+unit_price_service_charge = 312.24
+# *** ignore FT 5-10% and vat 7%
 
 df_load = pd.read_csv('analyse_electric_load_data.csv', parse_dates=['timestamp'])
 df_load.set_index('timestamp', inplace=True)
@@ -29,15 +36,15 @@ def cal_pv_serve_load(df_pv,df_load,pv_install_capacity):
 
 
     sum_pv_produce = df_load['pv_produce'].sum()
-    print(f"Energy of pv_produce: {sum_pv_produce:.2f} kWh/year")
-    print(f"Energy of pv_produce: {(sum_pv_produce/pv_install_capacity):.2f} kWh/kWp/year")
-    print(f"Energy of pv_produce: {(sum_pv_produce/pv_install_capacity/365):.2f} kWh/kWp/day")
+    print(f"Energy of pv_produce: {sum_pv_produce:,.2f} kWh/year")
+    print(f"Energy of pv_produce: {(sum_pv_produce/pv_install_capacity):,.2f} kWh/kWp/year")
+    print(f"Energy of pv_produce: {(sum_pv_produce/pv_install_capacity/365):,.2f} kWh/kWp/day")
 
     sum_pv_curtailed = df_load['pv_curtailed'].sum()
-    print(f"Energy of pv_curtailed: {sum_pv_curtailed:.2f} kWh  ({(sum_pv_curtailed/sum_pv_produce*100):.2f} %)")
+    print(f"Energy of pv_curtailed: {sum_pv_curtailed:,.2f} kWh  ({(sum_pv_curtailed/sum_pv_produce*100):.2f} %)")
 
     sum_pv_serve_load = df_load['pv_serve_load'].sum()
-    print(f"Energy of pv_serve_load: {sum_pv_serve_load:.2f} kWh")
+    print(f"Energy of pv_serve_load: {sum_pv_serve_load:,.2f} kWh")
 
 
 
@@ -63,13 +70,35 @@ def cal_pv_serve_load(df_pv,df_load,pv_install_capacity):
 
 
     sum_on_peak_data = on_peak_data['pv_serve_load'].sum()
-    print(f"  pv_serve_load -- On Peak: {sum_on_peak_data:.2f} kWh")
+    print(f"  pv_serve_load -- On Peak: {sum_on_peak_data:,.2f} kWh")
 
     sum_off_peak_data = off_peak_data['pv_serve_load'].sum()
-    print(f"  pv_serve_load -- Off Peak: {sum_off_peak_data:.2f} kWh")
+    print(f"  pv_serve_load -- Off Peak: {sum_off_peak_data:,.2f} kWh")
 
     sum_holiday_data = holiday_data['pv_serve_load'].sum()
-    print(f"  pv_serve_load -- holiday: {sum_holiday_data:.2f} kWh")
+    print(f"  pv_serve_load -- holiday: {sum_holiday_data:,.2f} kWh")
+    
+    demand_charge_df = df['pv_serve_load'].resample('M').max()
+    sum_demand_charge = demand_charge_df.sum()
+    # print(f"Sum of demand_charge: {sum_demand_charge:.2f} kW")
+    
+
+    price_on_peak = unit_price_on_peak * sum_on_peak_data
+    # print(f"price_on_peak: {price_on_peak:,.2f} THB")
+
+    price_off_peak = unit_price_off_peak * (sum_off_peak_data+sum_holiday_data)
+    # print(f"price_off_peak: {price_off_peak:,.2f} THB")
+
+    price_demand_charge = unit_price_demand_charge * sum_demand_charge
+    # print(f"price_demand_charge: {price_demand_charge:,.2f} THB")
+
+    price_service_charge = unit_price_service_charge * 12
+
+    total_price = price_on_peak + price_off_peak + price_demand_charge + price_service_charge
+    print(f"Total Base Price: {total_price:,.2f} THB")
+    print("\tignore FT & vat\n\r")
+        
+    
 
     pv_curtailed_kWh_df = df['pv_curtailed'].resample('D').sum()
     pv_curtailed_kWh_df = pv_curtailed_kWh_df[pv_curtailed_kWh_df > 1]
@@ -79,8 +108,8 @@ def cal_pv_serve_load(df_pv,df_load,pv_install_capacity):
     percentile_40_pv_curtailed_kWh = pv_curtailed_kWh_df.quantile(0.40)
     percentile_60_pv_curtailed_kWh = pv_curtailed_kWh_df.quantile(0.60)
     percentile_80_pv_curtailed_kWh = pv_curtailed_kWh_df.quantile(0.80)
-    print(f"max of PV curtailed: {max_pv_curtailed_kWh:.2f} kWh")
-    print(f"  -- suggest Battery Capacity: {percentile_60_pv_curtailed_kWh:.0f} kWh")
+    print(f"max of PV curtailed: {max_pv_curtailed_kWh:,.2f} kWh")
+    print(f"  -- suggest Battery Capacity: {percentile_60_pv_curtailed_kWh:,.0f} kWh")
 
     
 
@@ -111,12 +140,15 @@ def cal_pv_serve_load(df_pv,df_load,pv_install_capacity):
     # ============= SELECT BATTERY SIZE ============= #
     # =============================================== #
     # batt_cap_selected = percentile_60_pv_curtailed_kWh
-    batt_cap_selected = 50
+    batt_cap_selected = 200 # kWh
 
     batt_store_kWh_df = np.where(pv_curtailed_kWh_df > batt_cap_selected, batt_cap_selected, pv_curtailed_kWh_df)
     sum_batt_store_kWh = batt_store_kWh_df.sum()
-    print(f"  -- installed Battery       : {batt_cap_selected:.0f} kWh")
-    print(f"  -- suggest Battery Saving  : {(sum_batt_store_kWh*4):.0f} THB  ({(sum_batt_store_kWh*10*4/batt_cap_selected):.0f} THB/kWh/10years)")
+    price_batt_store = (sum_batt_store_kWh*4.1)
+    batt_arbitrage_kWh = batt_cap_selected*0.7*365*0.9*2 # batt performance 70%, 2 (4.1-2.1) THB/unit
+    total_price_batt = price_batt_store + batt_arbitrage_kWh
+    print(f"  -- installed Battery       : {batt_cap_selected:,.0f} kWh")
+    print(f"  -- suggest Battery Saving  : {total_price_batt:,.0f} THB  ({(total_price_batt*10/batt_cap_selected):,.0f} THB/kWh/10years)")
     print("Installing a battery will be worthwhile only if there is income from other conditions, such as the cost of damages when a power outage occurs.")
 
     df.to_csv('load_pv_profile.csv')
