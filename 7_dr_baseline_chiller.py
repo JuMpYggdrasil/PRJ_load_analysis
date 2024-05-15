fd1a315d11a79a49e7711444196334e9c25f7cff
JuMpYggdrasil <70381321+JuMpYggdrasil@users.noreply.github.com>
2024-05-15 09:07:29 +0700

    update baseline
    update baseline chiller

--- 7_dr_baseline_chiller.py
+++ 7_dr_baseline_chiller.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 import math
 
 # Loop through each day in the year (yyyy-mm-dd)
-start_date = pd.Timestamp('2022-01-11')
-end_date = pd.Timestamp('2022-01-12')
+start_date = pd.Timestamp('2022-02-15')
+end_date = pd.Timestamp('2022-12-31')
 
 # Function to calculate demand pattern for a specific day
-def calculate_demand_pattern_for_day(data, day, Xday=4, Yday=5):
+def calculate_demand_pattern_for_day(data, day, Xday=4, Yday=10):
     # Get the dates 10 days before the given day
     interested_dates = pd.date_range(end=day - pd.Timedelta(days=1), periods=Yday)
 
@@ -24,10 +24,40 @@ def calculate_demand_pattern_for_day(data, day, Xday=4, Yday=5):
     
     # Keep only the maximum X dates
     top_X_dates = sorted_dates[:Xday]
-    
+
     # Filter data for top X dates
     top_X_data = interested_data[interested_data['timestamp'].dt.date.isin(top_X_dates)]
-    # print(top_X_data)
+    # print(top_X_data['load'])
+    min_load_value = top_X_data['load'].min()
+    
+   
+    # i=1
+    # while min_load_value < 0.1:
+    #     # Get the dates 10 days before the given day
+    #     interested_dates = pd.date_range(end=day - pd.Timedelta(days=1), periods=Yday+i)
+
+    #     # Filter data for the range of interested_dates
+    #     interested_data = data[data['timestamp'].dt.date.isin(interested_dates.date)]
+
+
+
+    #     # Calculate the sum of load for each day in the interested dates
+    #     sum_load_per_day = interested_data.groupby(interested_data['timestamp'].dt.date)['load'].sum()
+        
+    #     # Sort interested dates by sum of load from max to min
+    #     sorted_dates = sum_load_per_day.sort_values(ascending=False).index
+        
+    #     # Keep only the maximum X dates
+    #     top_X_dates = sorted_dates[:Xday]
+
+    #     # Filter data for top X dates
+    #     top_X_data = interested_data[interested_data['timestamp'].dt.date.isin(top_X_dates)]
+        
+    #     min_load_value = top_X_data['load'].min()
+    #     print(top_X_data['load'])
+    #     i+=1
+    #     if i>20:
+    #         break
     
     # Calculate average load pattern for top X dates
     average_load_pattern = top_X_data.groupby(top_X_data['timestamp'].dt.hour)['load'].mean()
@@ -60,6 +90,7 @@ for day in pd.date_range(start=start_date, end=end_date):
     
     # Plot real pattern
     plt.plot(real_pattern['timestamp'].dt.hour, real_pattern['load'], label=f'Real Value (Day: {day})')
+    
 
 # Plot the demand pattern
 plt.title('Hourly Average Load Patterns')
@@ -68,6 +99,42 @@ plt.ylabel('Average load')
 plt.legend()
 plt.grid(True)
 plt.show()
+
+
+
+combined_baseline_values = []
+combined_real_values = []
+
+# plt.figure(figsize=(10, 6))
+    
+for day in pd.date_range(start=start_date, end=end_date):
+    # Filter data for the specific day
+    real_pattern = dataframe[dataframe['timestamp'].dt.date == day.date()]
+    real_energy = real_pattern['load'].sum()
+    
+    
+    
+    # Calculate demand pattern for the current day
+    baseline_pattern = calculate_demand_pattern_for_day(dataframe, day)
+    if (real_energy < 1):
+        i=0
+        while i < len(baseline_pattern.values):
+            baseline_pattern.values[i] = 0
+            i+=1
+
+    
+    # Extend the list of combined baseline values with the values of the current baseline pattern
+    combined_baseline_values.extend(baseline_pattern.values)
+    
+    
+    
+    combined_real_values.extend(real_pattern['load'])
+    
+    # # Plot baseline pattern
+    # plt.plot(baseline_pattern, linestyle='--', label=f'Baseline (Day: {day})')
+    
+    # # Plot real pattern
+    # plt.plot(real_pattern['timestamp'].dt.hour, real_pattern['load'], label=f'Real Value (Day: {day})')
 
 
 # Plot the combined baseline values
@@ -89,3 +156,10 @@ RMSE = math.sqrt(sum_error)
 sum_value = sum(combined_real_values)
 RRMSE = RMSE/sum_value*100
 print(f'RRMSE= {RRMSE} %')
+
+
+baseline_energy = sum(combined_baseline_values)
+print("baseline energy = {:,.2f} kWh/year".format(baseline_energy))
+real_energy = sum(combined_real_values)
+print("real energy = {:,.2f} kWh/year".format(real_energy))
+print("% = {:,.2f} kWh/year".format((real_energy-baseline_energy)/real_energy*100))
